2070dd553022cf4af94f2a82e37dc448ba88068a
amoghpanhale <amoghmpanhale@gmail.com>
2023-05-02 20:58:26 -0600

    Added Zoom functionality using ZoomControls

--- BikeMap/app/src/main/java/com/example/bikemap/MainActivity.java
+++ BikeMap/app/src/main/java/com/example/bikemap/MainActivity.java
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.pdf.PdfRenderer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.provider.DocumentsContract;
 import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
+public class MainActivity extends AppCompatActivity {
     // Request code for selecting a PDF document.
     private static final int PICK_PDF_FILE = 2;
     private static final int PICK_DIRECTORY = 3;
@@ -49,18 +59,6 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent, PICK_PDF_FILE);
     }
 
-
-    private void openDirectory(Uri uriToLoad) {
-        // Choose a directory using the system's file picker.
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-
-        // Optionally, specify a URI for the directory that should be opened in
-        // the system file picker when it loads.
-        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, uriToLoad);
-
-        startActivityForResult(intent, PICK_DIRECTORY);
-    }
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
         super.onActivityResult(requestCode, resultCode, resultData);
@@ -68,16 +66,13 @@ public class MainActivity extends AppCompatActivity {
         if (resultCode == Activity.RESULT_OK) {
             switch (requestCode) {
                 case PICK_PDF_FILE:
-                    // The result data contains a URI for the PDF document that
-                    // the user selected.
                     Uri pdfUri = null;
                     if (resultData != null) {
                         pdfUri = resultData.getData();
-                        // Do something with the PDF document.
-                        String pdfName = getFileNameFromUri(pdfUri);
-                        Log.w("PDF name", pdfName);
+                        convertFirstPageToJPEG(pdfUri);
                     }
                     break;
+
                 case PICK_DIRECTORY:
                     // The result data contains a URI for the directory that the
                     // user selected.
@@ -91,17 +86,40 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private String getFileNameFromUri(Uri uri) {
-        String fileName = null;
-        if (uri != null) {
-            ContentResolver resolver = getContentResolver();
-            Cursor cursor = resolver.query(uri, null, null, null, null);
-            if (cursor != null && cursor.moveToFirst()) {
-                fileName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                cursor.close();
-            }
-        }
-        return fileName;
-    }
+    private void convertFirstPageToJPEG(Uri pdfUri) {
+        try {
+            // Open the PDF document
+            ParcelFileDescriptor fileDescriptor = getContentResolver().openFileDescriptor(pdfUri, "r");
+            PdfRenderer renderer = new PdfRenderer(fileDescriptor);
+            System.out.println("PDF RENDERRRRRRRRRRR:" + renderer);
 
+            // Get the first page of the PDF
+            PdfRenderer.Page page = renderer.openPage(0);
+            System.out.println("FIRST PAGE" + page);
+
+            // Create a bitmap of the page
+            Bitmap bitmap = Bitmap.createBitmap(page.getWidth(), page.getHeight(), Bitmap.Config.ARGB_8888);
+            page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+            System.out.println("Bitmap" + bitmap);
+
+            // Save the bitmap as a JPEG file
+            String jpegName = "page_0.jpg";
+            File jpegFile = new File(getFilesDir(), jpegName);
+            FileOutputStream out = new FileOutputStream(jpegFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+            Log.i("Image Generation", "Image generation and storage successful. JPEG file path: " + jpegFile.getAbsolutePath());
+
+            Intent intent = new Intent(this, HomepageActivity.class);
+            intent.putExtra("image_path", jpegFile.getAbsolutePath());
+            startActivity(intent);
+
+            // Clean up
+            page.close();
+            renderer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
